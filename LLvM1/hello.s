b8f7584fc8ad24762ca616fbde74f5c0cac32eb8
ayushraina <ayushraina@iisc.ac.in>
2025-02-09 02:45:43 +0530

    LLvM Build Successfull and generated LLVM IR from C++ Code

---
+++ LLvM1/hello.s
@@ -0,0 +1,42 @@
+	.file	"hello.cpp"
+                                        # Start of file scope inline assembly
+	.globl	_ZSt21ios_base_library_initv
+
+                                        # End of file scope inline assembly
+	.text
+	.globl	main                            # -- Begin function main
+	.p2align	4
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movq	_ZSt4cout@GOTPCREL(%rip), %rdi
+	movabsq	$.L.str, %rsi
+	callq	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rsi
+	movq	%rax, %rdi
+	callq	_ZNSolsEPFRSoS_E@PLT
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object                  # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"Hello, LLVM "
+	.size	.L.str, 13
+
+	.ident	"clang version 21.0.0git (https://github.com/llvm/llvm-project.git cf5947be13e7af67219379e07bc0128f1f1e7f88)"
+	.section	".note.GNU-stack","",@progbits
